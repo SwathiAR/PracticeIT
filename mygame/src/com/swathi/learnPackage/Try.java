1010842a1db64a46e87af0354a3d50d5fa287c06
SwathiAR <swathi.bhat01@gmail.com>
2015-04-30 18:36:41 -0700

    myGame practice programs

---
+++ mygame/src/com/swathi/learnPackage/Try.java
@@ -0,0 +1,23 @@
+package com.swathi.learnPackage;
+import com.swathi.learnPackages.*;
+
+
+public class Try {
+
+	public static void main(String[] args) {
+		
+		Trypackages t = new Trypackages();
+		
+		
+	
+		
+		t.doThis();
+
+	}
+
+	public void doThat()
+	{
+		System.out.println("Executing doThat");
+	}
+
+}
---
+++ mygame/src/com/swathi/learnPackages/Trypackages.java
@@ -0,0 +1,17 @@
+package com.swathi.learnPackages;
+
+import static java.lang.System.out;
+
+
+public class Trypackages {
+
+	public static void main(String[] args) {
+		out.println("Try it again");
+
+	}
+
+	public void doThis()
+	{
+		out.println("Executing doThis");
+	}
+}
